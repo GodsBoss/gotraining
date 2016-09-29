fe699814b6acf40165674b866e9723d1c266f89d
Timo Reitz <godsboss@gmx.de>
2016-09-29 20:57:24 +0200

    Let server listen on a socket, accepting connections, but closing them immediately.

--- jsonconnect/server/main.go
+++ jsonconnect/server/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+  "fmt"
+  "github.com/godsboss/gotraining/jsonconnect/config"
+  "net"
+)
 
+func main() {
+  listener, err := net.Listen("tcp", "127.0.0.1:" + string(config.PORT))
+  if err != nil {
+    fmt.Println("Could not open socket connection: " + err.Error())
+    return
+  }
+  for {
+    connection, err := listener.Accept()
+    if err != nil {
+      fmt.Println("Error while accepting connections: " + err.Error())
+    } else {
+      go handleConnection(connection)
+    }
+  }
+}
+
+func handleConnection(connection net.Conn) {
+  connection.Close()
 }
