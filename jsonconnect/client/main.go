f9ae6861f5aa3716b93844c46b6cadfa5957eb1e
Timo Reitz <godsboss@gmx.de>
2016-09-29 21:07:08 +0200

    Enhance client: It tries to connect and throws an error if it fails.

--- jsonconnect/client/main.go
+++ jsonconnect/client/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+  "fmt"
+  "github.com/godsboss/gotraining/jsonconnect/config"
+  "net"
+)
 
+func main() {
+  _, err := net.Dial("tcp", "127.0.0.1:" + config.PORT)
+  if err != nil {
+    fmt.Println("Could not connect: " + err.Error())
+    return
+  }
 }
