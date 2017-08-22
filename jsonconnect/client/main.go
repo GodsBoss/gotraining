8172685f10628a13e8d02a782a3f072affc92ee7
Timo Reitz <godsboss@gmx.de>
2017-08-22 21:22:09 +0200

    Format code with gofmt.

--- jsonconnect/client/main.go
+++ jsonconnect/client/main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "github.com/godsboss/gotraining/jsonconnect/config"
-  "github.com/godsboss/gotraining/jsonconnect/data"
-  "io"
-  "net"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+
+	"github.com/godsboss/gotraining/jsonconnect/config"
+	"github.com/godsboss/gotraining/jsonconnect/data"
 )
 
 func main() {
-  connection, err := net.Dial("tcp", "127.0.0.1:" + config.PORT)
-  if err != nil {
-    fmt.Println("Could not connect: " + err.Error())
-    return
-  }
-  decodeAllData(connection)
+	connection, err := net.Dial("tcp", "127.0.0.1:"+config.PORT)
+	if err != nil {
+		fmt.Println("Could not connect: " + err.Error())
+		return
+	}
+	decodeAllData(connection)
 }
 
 func decodeAllData(reader io.ReadCloser) {
-  defer reader.Close()
-  decoder := json.NewDecoder(reader)
-  var value data.Data
-  for decoder.More() {
-    err := decoder.Decode(&value)
-    if err != nil {
-      fmt.Println("Could not decode value: " + err.Error())
-    } else {
-      fmt.Printf("Received value %d.\n", value.Number)
-    }
-  }
+	defer reader.Close()
+	decoder := json.NewDecoder(reader)
+	var value data.Data
+	for decoder.More() {
+		err := decoder.Decode(&value)
+		if err != nil {
+			fmt.Println("Could not decode value: " + err.Error())
+		} else {
+			fmt.Printf("Received value %d.\n", value.Number)
+		}
+	}
 }
--- jsonconnect/server/main.go
+++ jsonconnect/server/main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "github.com/godsboss/gotraining/jsonconnect/config"
-  "github.com/godsboss/gotraining/jsonconnect/data"
-  "net"
+	"encoding/json"
+	"fmt"
+	"net"
+
+	"github.com/godsboss/gotraining/jsonconnect/config"
+	"github.com/godsboss/gotraining/jsonconnect/data"
 )
 
 func main() {
-  listener, err := net.Listen("tcp", "127.0.0.1:" + config.PORT)
-  if err != nil {
-    fmt.Println("Could not open socket connection: " + err.Error())
-    return
-  }
-  for {
-    connection, err := listener.Accept()
-    if err != nil {
-      fmt.Println("Error while accepting connections: " + err.Error())
-    } else {
-      fmt.Println("Accepted connection from: " + connection.RemoteAddr().String())
-      go handleConnection(connection)
-    }
-  }
+	listener, err := net.Listen("tcp", "127.0.0.1:"+config.PORT)
+	if err != nil {
+		fmt.Println("Could not open socket connection: " + err.Error())
+		return
+	}
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error while accepting connections: " + err.Error())
+		} else {
+			fmt.Println("Accepted connection from: " + connection.RemoteAddr().String())
+			go handleConnection(connection)
+		}
+	}
 }
 
 func handleConnection(connection net.Conn) {
-  defer connection.Close()
-  encoder := json.NewEncoder(connection)
-  for i := 0; i<5; i++ {
-    encoder.Encode(data.GetRandom())
-  }
+	defer connection.Close()
+	encoder := json.NewEncoder(connection)
+	for i := 0; i < 5; i++ {
+		encoder.Encode(data.GetRandom())
+	}
 }
